0b47bb55ab822ffb49543015d7ff4e0cfab4691f
Smarpit <smarpitsingh560@gmail.com>
2020-09-30 00:36:36 +0530

    EditText

--- lib/main.dart
+++ lib/main.dart
@@ -3,30 +3,56 @@ import 'dart:html';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MyEditText());
 }
 
-class MyApp extends StatelessWidget {
+class MyEditText extends StatefulWidget {  // 1st
+
+  @override
+  MyEditTextState createState() => MyEditTextState();  // 2nd
+}
+
+class MyEditTextState extends State<MyEditText> {  // 3rd
+
+  String results = "";
+
+  final TextEditingController controller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // Title
-        title: "Simple Material App",
-        // Home
-        home: Scaffold(
-          // Appbar
-          appBar: AppBar(
-            // Title
-            title: Text("Simple Material App"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Using EditText"),
+        backgroundColor: Colors.red,
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(10.0),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              TextField(
+                decoration: InputDecoration(hintText: "Enter text here..."),
+                onSubmitted: (String str) {
+                  setState(() {
+                    results = results + "\n" + str;
+                    controller.text = "";
+                  });
+                },
+                controller: controller,
+              ),
+              Text(results)
+            ],
           ),
-          // Body
-          body: Container(
-            // Center the content
-            child: Center(
-              // Add Text
-              child: Text("Hello World!"),
-            ),
-          ),
-        ));
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+
+    controller.dispose();
+    super.dispose();
   }
 }
