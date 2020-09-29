58c960f50545d3c07cf408cd9d390ee1e816b7ab
Smarpit <smarpitsingh560@gmail.com>
2020-09-30 00:12:06 +0530

    Stateful

--- lib/main.dart
+++ lib/main.dart
@@ -6,27 +6,51 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {      // 1st
+  @override
+  MyStateFullWidget createState() => MyStateFullWidget();   // 2nd
+}
+
+class MyStateFullWidget extends State<MyApp> {   // 3rd
+
+  int _counter = 0;
+  List<String> _lis = ['Flutter', 'Is', "Cool", "and Awesome"];
+  String _displayedString = "Hello World!";
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // Title
-        title: "Simple Material App",
-        // Home
-        home: Scaffold(
-          // Appbar
-          appBar: AppBar(
-            // Title
-            title: Text("Simple Material App"),
-          ),
-          // Body
-          body: Container(
-            // Center the content
-            child: Center(
-              // Add Text
-              child: Text("Hello World!"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Stateful Example'),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(20),
             ),
-          ),
-        ));
+            Text(
+              _displayedString,
+              style: TextStyle(color: Colors.black),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10.0),
+            ),
+            RaisedButton(
+              textColor: Colors.black,
+              child: Text('Click Here'),
+              onPressed: buttonPressed,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  void buttonPressed() {
+    setState(() {
+      _counter = _counter < 4 ? _counter + 1 : 0;
+      _displayedString = _lis[_counter];
+    });
   }
 }
